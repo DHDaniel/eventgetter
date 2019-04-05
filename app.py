76d6af79b7fe3474210a817de37e836089e4f571
Daniel Hernandez <dh.danny@hotmail.com>
2019-04-05 14:11:19 -0700

    Add eventbrite shite

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, session, request, render_template, redirect, url_for
+import requests
+import os
+import json
+
 app = Flask(__name__)
 
 app.config.from_envvar("APP_SETTINGS")
@@ -15,4 +19,15 @@ def index():
 @app.route('/login')
 def login():
     print(request.args)
-    return render_template('login.html')
+    if "access_token" in request.args:
+        EVENTBRITE_SECRET = os.environ.get("EVENTBRITE_SECRET")
+        EVENTBRITE_APP_KEY = os.environ.get("EVENTBRITE_APP_KEY")
+        data = {code: request.args["access_token"],
+                client_secret: EVENTBRITE_SECRET,
+                client_id: EVENTBRITE_APP_KEY,
+                grant_type: "authorization_code"}
+        r = requests.post("https://www.eventbrite.com/oauth/token", data=data)
+        response = r.json()
+        print(response)
+    else:
+        return render_template('login.html')
